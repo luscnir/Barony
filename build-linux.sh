69092f2cac7f025fa31980bd72100479b49afcb1
James Browning <thejamesernator@gmail.com>
2020-12-28 21:02:04 +1300

    .

--- build-linux.sh
+++ build-linux.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-steamworksDir="$(readlink -f $(dirname $0))/steamworks"
+
+steamworksDir="$(readlink -f "$STEAMWORKSROOT")"
 
 rm -rf build/
 mkdir -p build/
 cd build/
+
 
 STEAMWORKS_ENABLED=1 STEAMWORKSROOT="$steamworksDir" cmake \
   -DCMAKE_BUILD_TYPE=Release \
